d6029300878bd80b02600c04fc694f3e969a102f
Hangyu Ye <jerry.hangyu@gmail.com>
2018-12-13 21:55:34 +0800

    Update sync.sh

--- miner/sync.sh
+++ miner/sync.sh
@@ -1,6 +1,8 @@
 #!/usr/bash
 echo "Syncing block, it won't take too long"
 
-#Sync Block Date with 12hours snapshot
-curl https://s3-us-west-2.amazonaws.com/testnet2/data/22/$(curl -s https://s3-us-west-2.amazonaws.com/testnet2/data/22/LATEST).tar.gz --output data.tar.gz && tar xvfz data.tar.gz
-docker run -v /path/to/data:/code/pyquarkchain/quarkchain/cluster/data -it qkcminer
+docker exec -it qkcminer pypy3 quarkchain/cluster/cluster.py --cluster_config /code/pyquarkchain/testnet/2/cluster_config_template.json
+
+# Sync Block Date with 12hours snapshot
+# curl https://s3-us-west-2.amazonaws.com/testnet2/data/22/$(curl -s https://s3-us-west-2.amazonaws.com/testnet2/data/22/LATEST).tar.gz --output data.tar.gz && tar xvfz data.tar.gz
+# docker run -v /path/to/data:/code/pyquarkchain/quarkchain/cluster/data -it qkcminer
