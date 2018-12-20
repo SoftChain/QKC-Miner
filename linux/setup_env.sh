6ebd4d33d764882581fb271395ae61b03074e87f
Hangyu Ye <jerry.hangyu@gmail.com>
2018-12-20 18:43:03 +0800

    Add Cleaning tool

--- linux/setup_env.sh
+++ linux/setup_env.sh
@@ -10,7 +10,8 @@ sudo apt install docker.io
 echo Pulling Shell Script
 git clone https://github.com/HangyuYe/QKC-Miner.git
 cd QKC-Miner
+git pull
 
 echo Pulling image
 sudo docker pull ninjaahhh/pyquarkchain:testnet2.3.1
-sudo docker run -it --name qkcminer -p 38291:38291 -p 38391:38391 -p 38491:38491 -p 38291:38291/udp ninjaahhh/pyquarkchain:testnet2.3.0 bash /code/pyquarkchain/quarkchain/tools/quick_miner_tool.sh
+sudo docker run -it --name qkcminer -p 38291:38291 -p 38391:38391 -p 38491:38491 -p 38291:38291/udp ninjaahhh/pyquarkchain:testnet2.3.1 bash /code/pyquarkchain/quarkchain/tools/quick_miner_tool.sh
